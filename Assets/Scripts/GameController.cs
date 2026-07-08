412560c5387bc287b219219aa0a9de9b44a7d80d
Roberto Bevilaqua <robertobevilaqua98@gmail.com>
2026-07-07 22:22:15 -0400

    IntroScene

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController gc;
+
+    void Awake()
+    {
+        if(gc == null) gc = this;
+        else Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+}
---
+++ Assets/Scripts/Scenario/LightControl.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class LightControl : MonoBehaviour
+{
+    #region Variables
+    [SerializeField] private Light2D topLight;
+    [SerializeField] private Light2D centerLight;
+    [SerializeField] private Light2D bottomLight;
+
+    private Camera cam;
+
+    #endregion
+
+    void Awake()
+    {
+        cam = Camera.main;
+        LightAdjustOnCamera();
+        IntroLayout();
+    }
+
+    private void LightAdjustOnCamera()
+    {
+        topLight.pointLightOuterRadius = cam.orthographicSize * .7f;
+        centerLight.pointLightOuterRadius = cam.orthographicSize * .7f;
+        bottomLight.pointLightOuterRadius = cam.orthographicSize * .7f;
+
+        topLight.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y + cam.orthographicSize, 0);
+        centerLight.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y, 0);
+        bottomLight.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y - cam.orthographicSize, 0);
+    }
+
+    private void IntroLayout()
+    {
+        topLight.enabled = true;
+        centerLight.enabled = false;
+        bottomLight.enabled = true;
+    }
+
+    private void DefaultLayout()
+    {
+        topLight.enabled = true;
+        centerLight.enabled = false;
+        bottomLight.enabled = false;
+    }
+
+    private void CountdownLayout()
+    {
+        topLight.enabled = false;
+        centerLight.enabled = true;
+        bottomLight.enabled = false;
+    }
+
+}
